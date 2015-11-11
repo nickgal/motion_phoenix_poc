e0024752573fea6dd34ed0a76a0ea2afd9de2ebd
Nick Gal <nickgal@me.com>
2015-11-10 18:50:34 -0800

    hello world watchos2 + ib

--- homemade_pi_watch/app/interface_controller.rb
+++ homemade_pi_watch/app/interface_controller.rb
@@ -1,9 +1,13 @@
 class InterfaceController < WKInterfaceController
+  extend IB
+
+  outlet :hello_world, WKInterfaceLabel
 
   def awakeWithContext(context)
     super
 
     # Initialize variables here.
+    @hello_world.text = "hellow"
     # Configure interface objects here.
     NSLog("%@ awakeWithContext", self)
 
