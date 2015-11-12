c97a85a5a8512fa3707bb068883fdc9f4e973197
Nick Gal <nickgal@me.com>
2015-11-12 09:13:37 -0800

    load switch data via network

--- watch_app/app/interface_controller.rb
+++ watch_app/app/interface_controller.rb
@@ -7,22 +7,25 @@ class InterfaceController < WKInterfaceController
     super
 
     # Initialize variables here.
-    # Switch.async_load do |switches|
-    switches = [
-      {name: 'xmas tree', state: false},
-      {name: 'xmas lights', state: false},
-      {name: 'plant heater', state: false},
-      {name: 'plant lights', state: true},
-      {name: 'living room light', state: false}
-    ]
-      @switches_table.setNumberOfRows(switches.length, withRowType: "SwitchRow")
-      switches.each_with_index do |switch, index|
-        row = @switches_table.rowControllerAtIndex(index)
-        row.switch_item.setTitle switch[:name]
-        row.switch_item.setOn switch[:state]
-        # mp row
+    url = NSURL.URLWithString "http://192.168.101.106:4000/api/switches"
+    conf = NSURLSessionConfiguration.defaultSessionConfiguration
+    session = NSURLSession.sessionWithConfiguration conf
+    task = session.dataTaskWithURL(url, completionHandler: lambda do |data, res, error|
+      if error
+        NSLog("error? %@", error)
+      else
+        json = NSJSONSerialization.JSONObjectWithData data, options: 0, error: nil
+        switches = json["data"]
+        @switches_table.setNumberOfRows(switches.length, withRowType: "SwitchRow")
+        switches.each_with_index do |switch, index|
+          row = @switches_table.rowControllerAtIndex(index)
+          row.switch_item.setTitle switch["name"]
+          row.switch_item.setOn switch["state"]
+          # mp row
+        end
       end
-    # end
+    end)
+    task.resume
     # Configure interface objects here.
     NSLog("%@ awakeWithContext", self)
 
